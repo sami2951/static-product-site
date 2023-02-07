8a28b1184d0afc1d6c801a346f1fd5e97d7bb2e4
Sami Larsen <sami.larsen@outlook.com>
2023-02-07 14:14:11 +0100

    productlist virker med product

--- js/product.js
+++ js/product.js
@@ -1,4 +1,8 @@
-fetch("https://kea-alt-del.dk/t7/api/products/1528")
+
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get("id");
+
+fetch("https://kea-alt-del.dk/t7/api/products/"+id)
 .then((response) => response.json())
 .then((data) => showProduct(data));
 
